7e1b414729d0e9ab473557fe1a8452dfeff9d0b9
Kevin Morris <morrisk@brennan.law.nyu.edu>
2022-03-02 18:16:11 -0500

    add biden voteshare

--- code/03_state_stuff.R
+++ code/03_state_stuff.R
@@ -59,7 +59,7 @@ census <- left_join(census, comp, by = c("NAME" = "state")) %>%
 m1ab <- lm(intro ~ poly(nh_white, 2) + FinalCOVI, census)
 
 m1a <- lm(intro ~ poly(nh_white, 2) + FinalCOVI +
-            change_dem + dem_share_2020 +
+            change_dem + poly(dem_share_2020, 2) +
             median_income + median_age + population +
             some_college, census)
 
@@ -85,7 +85,7 @@ p1
 m2ab <- lm(passed ~ poly(nh_white, 2) + FinalCOVI, census)
 
 m2a <- lm(passed ~ poly(nh_white, 2) + FinalCOVI +
-            change_dem + dem_share_2020 +
+            change_dem + poly(dem_share_2020, 2) +
             median_income + median_age + population +
             some_college, census)
 
@@ -121,18 +121,19 @@ ggdraw(add_sub(p, "   Covariates include median income; median age; change in De
 m1aa <- lm(intro ~ poly(nh_white, 2) * FinalCOVI, census)
 
 m1 <- lm(intro ~ poly(nh_white, 2) * FinalCOVI +
-           change_dem + dem_share_2020 +
+           change_dem + poly(dem_share_2020, 2) +
            median_income + median_age + population +
            some_college, census)
 
 p <- paste(seq(min(census$nh_white),
                max(census$nh_white), 0.01), collapse = ", ")
 
-marg <- ggeffect(m1, terms = c(paste0("nh_white[", p, "]"), "FinalCOVI[-1, 0, 1]"))
+marg <- ggeffect(m1, terms = c(paste0("nh_white[", p, "]"), "FinalCOVI[-.5, .5]"))
 
 p1 <- ggplot(marg, aes(x = x, y = predicted,
                        color = group, fill = group,
-                       ymin = conf.low, ymax = conf.high)) +
+                       ymin = conf.low, ymax = conf.high,
+                       linetype = group)) +
   geom_line() +
   geom_ribbon(alpha = 0.2) +
   theme_bc(legend.position = "none") +
@@ -141,7 +142,8 @@ p1 <- ggplot(marg, aes(x = x, y = predicted,
   labs(x = "White Share of Population",
        y = "Restrictive Provisions Introduced",
        color = "2020 COVI",
-       fill = "2020 COVI") +
+       fill = "2020 COVI",
+       linetype = "2020 COVI") +
   ggtitle("(A) Provisions Introduced")
 
 ggsave("temp/state_intro.png")
@@ -149,15 +151,16 @@ ggsave("temp/state_intro.png")
 m2aa <- lm(passed ~ poly(nh_white, 2) * FinalCOVI, census)
 
 m2 <- lm(passed ~ poly(nh_white, 2) * FinalCOVI +
-           change_dem + dem_share_2020 +
+           change_dem + poly(dem_share_2020, 2) +
            median_income + median_age + population +
            some_college, census)
 
-marg <- ggeffect(m2, terms = c(paste0("nh_white[", p, "]"), "FinalCOVI[-1, 0, 1]"))
+marg <- ggeffect(m2, terms = c(paste0("nh_white[", p, "]"), "FinalCOVI[-.5, .5]"))
 
 p2 <- ggplot(marg, aes(x = x, y = predicted,
                        color = group, fill = group,
-                       ymin = conf.low, ymax = conf.high)) +
+                       ymin = conf.low, ymax = conf.high,
+                       linetype = group)) +
   geom_line() +
   geom_ribbon(alpha = 0.2) +
   theme_bc(legend.position = "none") +
@@ -166,7 +169,8 @@ p2 <- ggplot(marg, aes(x = x, y = predicted,
   labs(x = "White Share of Population",
        y = "Restrictive Provisions Passed",
        color = "2020 COVI",
-       fill = "2020 COVI") +
+       fill = "2020 COVI",
+       linetype = "2020 COVI") +
   ggtitle("(B) Provisions Passed")
 
 legend_b <- get_legend(
@@ -181,33 +185,77 @@ p <- plot_grid(p1, p2,
 
 j <- plot_grid(p, legend_b, ncol = 1, rel_heights = c(1, .1))
 
-ggdraw(add_sub(j, "   Covariates include median income; median age; change in Democratic voteshare, 2016--2020;
+f <- ggdraw(add_sub(j, "   Covariates include median income; median age; change in Democratic voteshare, 2016--2020;
    Biden voteshare, 2020; share with some college; log(population).", x = 0, hjust = 0,
                fontfamily = "BentonSans", size = 12))
+saveRDS(f, "temp/mef_state_good.rds")
+models <- list(m1ab,
+               m1a,
+               m2ab,
+               m2a,
+               m1aa,
+               m1,
+               m2aa,
+               m2)
 
-modelsummary(list("Introduced" = m1ab, "Passed" = m2ab,
-                  "Introduced" = m1a, "Passed" = m2a,
-                  "Introduced" = m1aa, "Passed" = m2aa,
-                  "Introduced" = m1, "Passed" = m2),
+j <- rbindlist(lapply(c(1:length(models)), function(i){
+  
+  m <- models[[i]]
+  k <-  linearHypothesis(m, c("poly(nh_white, 2)1", "poly(nh_white, 2)2"))[2,6]
+  star <- ifelse(k < 0.01, "***",
+                 ifelse(k < 0.05, "**",
+                        ifelse(k < 0.1, "*", "")))
+  k <- paste0(ifelse(k < .001, "< 0.001", as.character(round(k, 3))), star)
+  
+  
+  if(i %% 2 == 0){
+    l <-  linearHypothesis(m, c("poly(dem_share_2020, 2)1", "poly(dem_share_2020, 2)2"))[2,6]
+    star <- ifelse(l < 0.01, "***",
+                   ifelse(l < 0.05, "**",
+                          ifelse(l < 0.1, "*", "")))
+    l <- paste0(ifelse(l < .001, "< 0.001", as.character(round(l, 3))), star)
+  }else{
+    l <- ""
+  }
+  return(data.table(w = k, b = l, m = i))
+})) %>% 
+  pivot_longer(cols = c(b, w)) %>% 
+  pivot_wider(id_cols = name, values_from = value, names_from = m) %>% 
+  arrange(rev(name))
+
+j <- rbind(data.table(V0 = "\\textit{p}-value of Joint Significance Test"),
+           cbind(data.table(V0 = "on Nonhispanic White Terms"),
+                 setDT(as.list(j))[]), fill = T) %>% 
+  mutate(across(everything(), ~ ifelse(is.na(.), "", .)))
+attr(j, 'position') <- c(25, 26)
+
+modelsummary(models,
              statistic = "std.error",
-             stars = c("*" = 0.05, "**" = 0.01, "***" = 0.001),
+             stars = c("*" = 0.1, "**" = 0.05, "***" = 0.01),
              gof_omit = 'DF|Deviance|AIC|BIC|Within|Pseudo|Log|Std|FE|F',
              coef_map = c("poly(nh_white, 2)1" = "Nonhispanic White",
                           "poly(nh_white, 2)2" = "Nonhispanic White\\textsuperscript{2}",
                           "FinalCOVI" = "2020 COVI",
                           "poly(nh_white, 2)1:FinalCOVI" = "Nonhispanic White $\\times$ 2020 COVI",
                           "poly(nh_white, 2)2:FinalCOVI" = "Nonhispanic White\\textsuperscript{2} $\\times$ 2020 COVI",
+                          "poly(dem_share_2020, 2)1" = "Biden Voteshare, 2020",
+                          "poly(dem_share_2020, 2)2" = "Biden Voteshare, 2020\\textsuperscript{2}",
                           "change_dem" = "Change in Dem. Vote Share 2016--2020",
-                          "dem_share_2020" = "Biden Voteshare, 2020",
                           "median_income" = "Median Income (\\$10,000s)",
                           "median_age" = "Median Age",
                           "some_college" = "Share with Some College",
                           "population" = "Log(Population)",
                           "(Intercept)" = "Intercept"),
-             output = "test.tex",
-             escape = F)
+             escape = F,
+             output = "latex",
+             title = "\\label{tab:state} State-Level Restrictive Provisions, 2021",
+             add_rows = j) %>% 
+  kable_styling(latex_options = "scale_down") %>% 
+  add_header_above(c("", "Introduced Provisions" = 2, "Passed Provisions" = 2,
+                     "Introduced Provisions" = 2, "Passed Provisions" = 2)) %>% 
+  save_kable("temp/state_reg.tex")
 
-###############
+
 cleanup("state_level")
 
 types <- state_level %>% 
@@ -325,3 +373,58 @@ saveRDS(map2, "temp/map_passed.rds")
 gr <- plot_grid(map1, map2)
 gr
 saveRDS(gr, "temp/map_provs.rds")
+
+######################################
+######################################
+######################################
+
+m1 <- lm(intro ~ poly(dem_share_2020, 2), census)
+
+m2 <- lm(passed ~ poly(dem_share_2020, 2), census)
+
+p <- paste(seq(min(census$dem_share_2020),
+               max(census$dem_share_2020), 0.01), collapse = ", ")
+
+marg <- ggeffect(m1, terms = c(paste0("dem_share_2020[", p, "]")))
+
+p1 <- ggplot(marg, aes(x = x, y = predicted,
+                       ymin = conf.low, ymax = conf.high)) +
+  geom_line() +
+  geom_ribbon(alpha = 0.2) +
+  theme_bc(legend.position = "none") +
+  scale_x_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::comma) +
+  labs(x = "Biden Voteshare 2020",
+       y = "Restrictive Provisions Introduced") +
+  ggtitle("(A) Provisions Introduced")
+
+marg <- ggeffect(m2, terms = c(paste0("dem_share_2020[", p, "]")))
+
+p2 <- ggplot(marg, aes(x = x, y = predicted,
+                       ymin = conf.low, ymax = conf.high)) +
+  geom_line() +
+  geom_ribbon(alpha = 0.2) +
+  theme_bc(legend.position = "none") +
+  scale_x_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::comma) +
+  labs(x = "Biden Voteshare 2020",
+       y = "Restrictive Provisions Passed") +
+  ggtitle("(B) Provisions Passed")
+
+legend_b <- get_legend(
+  p1 + theme(legend.position = "bottom")
+)
+
+p <- plot_grid(p1, p2,
+               label_size = 12,
+               label_fontfamily = "LM Roman 10")
+
+j <- plot_grid(p, legend_b, ncol = 1, rel_heights = c(1, .1))
+
+f <- ggdraw(add_sub(j, "   Covariates include share nonhispanic white; 2020 COVI; median income; median age;
+   change in Democratic voteshare, 2016--2020; share with some college; log(population).", x = 0, hjust = 0,
+                    fontfamily = "BentonSans", size = 12))
+
+f
+
+linearHypothesis(m1, c("poly(dem_share_2020, 2)1", "poly(dem_share_2020, 2)2"))
